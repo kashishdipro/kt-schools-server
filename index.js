d742f741a1ad0f6409afae8840d48e8ce3a59dfa
kashishdipro <kamanashishdatta@gmail.com>
2022-10-27 22:54:38 +0600

    added details course information

--- index.js
+++ index.js
@@ -5,15 +5,34 @@ const port = process.env.PORT ||5000
 
 app.use(cors())
 
-const courses = require('./courses.json')
+const allcourse = require('./courses.json')
+const courses = require('./course.json')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
-})
+});
+
 app.get('/courses', (req, res) => {
+    res.send(allcourse)
+});
+
+app.get('/course', (req, res) => {
     res.send(courses)
-})
+});
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id
+    const filterCourse = courses.filter(c => c.course_id === id);
+    res.send(filterCourse);
+});
+
+app.get('/select/:id', (req, res) => {
+    const id = req.params.id
+    const selectCourse = courses.find(c => c._id === id);
+    res.send(selectCourse);
+});
+
 
 app.listen(port, () => {
     console.log(`KTschools running on ${port}`)
-})
+});
