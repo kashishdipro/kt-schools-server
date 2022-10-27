139da629251440e587a0e5dd5d4db4cd7ed655fa
kashishdipro <kamanashishdatta@gmail.com>
2022-10-28 00:12:41 +0600

    get information from course file

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT ||5000
 app.use(cors())
 
 const allcourse = require('./courses.json')
-const courses = require('./course.json')
+const courses = require('./course')
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
@@ -19,6 +19,19 @@ app.get('/courses', (req, res) => {
 app.get('/course', (req, res) => {
     res.send(courses)
 });
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id
+    const filterCourse = courses.filter(c => c.course_id === id);
+    res.send(filterCourse);
+});
+
+app.get('/select/:id', (req, res) => {
+    const id = req.params.id
+    const selectCourse = courses.find(c => c._id === id);
+    res.send(selectCourse);
+});
+
 
 app.listen(port, () => {
     console.log(`KTschools running on ${port}`)
